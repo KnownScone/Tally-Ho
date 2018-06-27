0556339d0c5ad42c5a0c6254a3136e323fd7a824
Zachary Newsom <knownsconegames@gmail.com>
2018-06-26 21:02:51 -0700

    Some utility functions and bug fixing for Rect

--- src/utility.rs
+++ src/utility.rs
@@ -1,4 +1,4 @@
-use cgmath::{Vector2, Vector3};
+use cgmath::{BaseNum, Vector2, Vector3};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Rect2<S> {
@@ -6,12 +6,23 @@ pub struct Rect2<S> {
     pub max: Vector2<S>, 
 }
 
-impl<S> Rect2<S> {
+impl<S: BaseNum> Rect2<S> {
     pub fn new(min: Vector2<S>, max: Vector2<S>) -> Self {
+        assert!(max.x > min.x && max.y > min.y, "Min must be smaller than max.");
+
         Rect2 {
             min,
             max
         }
+    }
+
+    pub fn extend(self, min_z: S, max_z: S) -> Rect3<S> {
+        Rect3::new(self.min.extend(min_z), self.max.extend(max_z))
+    }
+
+    pub fn is_intersecting(self, other: Self) -> bool {
+        (self.min.x <= other.max.x && self.max.x >= other.min.x) &&
+        (self.min.y <= other.max.y && self.max.y >= other.min.y)
     }
 }
 
@@ -21,11 +32,21 @@ pub struct Rect3<S> {
     pub max: Vector3<S>, 
 }
 
-impl<S> Rect3<S> {
+impl<S: BaseNum> Rect3<S> {
     pub fn new(min: Vector3<S>, max: Vector3<S>) -> Self {
+        assert!(max.x > min.x && max.y > min.y && max.z > min.z, "Min must be smaller than max.");
         Rect3 {
             min,
             max
         }
+    }
+
+    pub fn truncate(self) -> Rect2<S> {
+        Rect2::new(Vector2::new(self.min.x, self.min.y), Vector2::new(self.max.x, self.max.y))
+    }
+
+    pub fn is_intersecting(self, other: Self) -> bool {
+        (self.min.x <= other.max.x && self.max.x >= other.min.x) &&
+        (self.min.y <= other.max.y && self.max.y >= other.min.y)
     }
 }
